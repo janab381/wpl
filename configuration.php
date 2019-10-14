e2e5b397e167189ac57afa4360849e39465d2189
janab381 <53029534+janab381@users.noreply.github.com>
2019-10-15 00:55:50 +0500

    Inital
    commit one

---
+++ configuration.php
@@ -0,0 +1,12 @@
+<?php
+
+$XCStreamHostUrl = 'http://{{YourDNS}}:{{YourIP}}';
+$XClogoLinkval = 'images/iptv-premium-logo-200.png';
+$XCcopyrighttextval = '';
+$XCcontactUslinkval = '';
+$XChelpLinkval = '';
+$XClicenseIsval = 'Web-384884f8a2';
+$XClocalKey = '';
+$XCsitetitleval = 'Player TV';
+
+?> 
